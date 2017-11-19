d1cfb4ddd3247b6662cfa00a4e2edec8662d64b0
directimage <directimage@gmail.com>
2017-11-19 16:24:26 -0500

    Initial commit

---
+++ scripts/side-slideshow.js
@@ -0,0 +1,21 @@
+function startSideSlider() {
+	var images = $('.slide');
+	var index = Math.floor(Math.random() * images.length) % images.length;
+	var curr;
+	var next;
+
+	$('.side-bg.top').find('img').attr('src', $('#slideshow').find('img')[(index + 1) % images.length].src);
+	$('.side-bg.bottom').find('img').attr('src', $('#slideshow').find('img')[index].src);
+	
+	setInterval(() => {
+		curr = images[index];
+		index = (index + 1) % images.length;
+		next = images[index];
+		
+		$('.side-bg.top').find('img').attr("src", curr.src);
+		$('.side-bg.bottom').find('img').attr("src", next.src);
+
+		$('.side-bg.top').removeClass('hide');
+		setTimeout(() => { $('.side-bg.top').addClass('hide') }, 1);
+	}, 2600);
+}
